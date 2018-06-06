7bd5b0b535de4e5a89664f9ad26f55937cb32a95
musicode <musicode@qq.com>
2018-06-06 11:29:06 +0800

    update

--- src/node/Node.js
+++ src/node/Node.js
@@ -1,4 +1,6 @@
 
+import stringifyJSON from 'yox-common/function/stringifyJSON'
+
 import * as string from 'yox-common/util/string'
 
 /**
@@ -9,6 +11,10 @@ export default class Node {
   constructor(type, raw) {
     this.type = type
     this.raw = string.trim(raw)
+  }
+
+  stringify() {
+    return stringifyJSON(this)
   }
 
 }
